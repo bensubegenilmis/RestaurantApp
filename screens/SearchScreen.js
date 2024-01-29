0ed93e021b8b96f9743b42911a0f082ef882bb80
Bensu Beğenilmiş <bensubegenilmiss@gmail.com>
2024-01-29 16:26:47 +0300

    new

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,35 +1,46 @@
 import { StyleSheet, Text, View } from 'react-native';
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import SearchBar from '../components/SearchBar';
 import useResults from '../hooks/useResults';
 import ResultList from '../components/ResultList';
 
 export default function SearchScreen() {
-  const [searchApi,results, errorMessage] = useResults();
+  const [searchApi, results, errorMessage] = useResults();
   //console.log(results);
   const [term, setTerm] = useState('');
 
-  
+
   const filterResultsByPrice = (price) => {
-    return results.filter((results) =>{
-      return results.price === price;
+    return results.filter((result) => {
+      return result.price === price;
     });
   };
 
   return (
     <View>
       <SearchBar
-       term={term}
-       onTermChange={setTerm}
-       OnTermSubmit={() => searchApi (term)}
-       />
-      {errorMessage ? <Text>{errorMessage}</Text> : null }
-      <ResultList title = 'Ucuz Restaurantlar'
-      results={filterResultsByPrice('₺')} />
-      <ResultList title = 'Uygun Restaurantlar'
-      results={filterResultsByPrice('₺₺')} />
-      <ResultList title = 'Pahalı Restaurantlar'
-      results={filterResultsByPrice('₺₺₺')} />
+        term={term}
+        onTermChange={setTerm}
+        OnTermSubmit={() => searchApi(term)}
+      />
+      {errorMessage ? <Text>{errorMessage}</Text> :  
+      <>
+        {results.length == 0 ? (
+        <Text></Text>
+      ) : (
+        <>
+          <ResultList title='Ucuz Restaurantlar'
+            results={filterResultsByPrice('₺')} />
+          <ResultList title='Uygun Restaurantlar'
+            results={filterResultsByPrice('₺₺')} />
+          <ResultList title='Pahalı Restaurantlar'
+            results={filterResultsByPrice('₺₺₺')} /> 
+         </>
+         )}
+         </>
+         }
+      
+
     </View>
   );
 };
